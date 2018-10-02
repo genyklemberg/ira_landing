741cfac6102dd587c8cc13610bc7af4233156289
Volodymyr <z_valdemar@ukr.net>
2018-10-02 13:55:11 +0300

    fix mobile view - add horizontal scroll

--- src/app/views/home/home-one.component.ts
+++ src/app/views/home/home-one.component.ts
@@ -5,15 +5,13 @@ import { LandingFixService } from '../../shared/services/landing-fix.service';
   selector: 'app-home-one',
   template: `<app-header></app-header>
   <app-intro></app-intro>
-  <!--<app-modal-gallery></app-modal-gallery>-->
   <app-portfolio></app-portfolio>
   <app-cta></app-cta>
   <app-works></app-works>
   <app-services [backgroundGray]="true"></app-services>
   <app-pricings></app-pricings>
   <app-contact [backgroundGray]="true"></app-contact>
-  <app-footer></app-footer>
-  <!--<app-testimonials-carousel></app-testimonials-carousel>-->`
+  <app-footer></app-footer>`
 })
 export class HomeOneComponent implements OnInit, OnDestroy {
   constructor(
--- src/app/views/home/intro-one/intro-one.component.ts
+++ src/app/views/home/intro-one/intro-one.component.ts
@@ -6,22 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro-one.component.scss']
 })
 export class IntroOneComponent implements OnInit {
-  status = 'Mute';
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-  pauseAudio() {
-    const video = document.getElementById('myVideo') as HTMLMediaElement;
-
-    if (video.muted) {
-      video.muted = false;
-      this.status = 'Mute';
-    } else {
-      video.muted = true;
-      this.status = 'Unmute';
-    }
-  }
+  ngOnInit() {}
 }
