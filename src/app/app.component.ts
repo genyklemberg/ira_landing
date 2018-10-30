06ef051d25e132333ec407f7e189daf03dc686e6
genyklemberg <y.potupa@gmail.com>
2018-10-30 18:24:35 +0200

    analytics and SEO configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor( ) { }
+  constructor(private meta: Meta,
+              private title: Title) {
+    title.setTitle('котеджі на Куликівській - коли все поруч');
+    meta.addTags([
+      { name: 'author',   content: 'kulikivska.lviv.ua'},
+      { name: 'type', content: 'website'},
+      { name: 'description', content: 'Котеджі на Куликівській - 6 спарених просторих котеджів в парковому районі Львова від забудовника. Ми створили для вас затишний комплекс в одному з найближчих до центру спальних районів. Сучасне оформлення, сертифіковані матеріали будівництва, закрита територія та розвинена інфраструктура - створять для вас справжній сімейний затишок.' },
+      { name: 'image', content: 'https://kulikivska.lviv.ua/assets/images/new/DSC_0055.jpg' },
+      { name: 'og:author',   content: 'kulikivska.lviv.ua'},
+      { name: 'og:type', content: 'website'},
+      { name: 'og:description', content: 'Котеджі на Куликівській - 6 спарених просторих котеджів в парковому районі Львова від забудовника. Ми створили для вас затишний комплекс в одному з найближчих до центру спальних районів. Сучасне оформлення, сертифіковані матеріали будівництва, закрита територія та розвинена інфраструктура - створять для вас справжній сімейний затишок.' },
+      { name: 'og:image', content: 'https://kulikivska.lviv.ua/assets/images/new/DSC_0055.jpg' }
+    ]);
+  }
 
   ngOnInit() {
     
